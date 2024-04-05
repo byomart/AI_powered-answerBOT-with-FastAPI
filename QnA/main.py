c3d95c71a857d7683e7b9fffef7d9490d04feacd
fbayomartinez <fbayomartinez@gmail.com>
2024-04-05 10:12:35 +0000

    with fast api implemented

--- QnA/main.py
+++ QnA/main.py
@@ -5,7 +5,6 @@ from answr_DistilBert import DistilBertAnswerGenerator
 from answr_RoBERTa import RoBERTaAnswerGenerator
 from fastapi import FastAPI
 
-
 import logging
 logging.basicConfig(level=logging.INFO, 
                     filename="log.log", 
@@ -18,33 +17,67 @@ app = FastAPI()
 def read_root():
     return {"message": "Welcome to the Q&A API!"}
 
-# user input
-query = input("Any question? ")
+@app.get("/answers")
+def Question_Answer(query: str):
+    try:
+        kw = Keywords()
+        keywords = kw.extract_keywords(query)
 
-# keyword extraction from user input
-kw = Keywords()
-keywords = kw.extract_keywords(query)
+        collector = Info_Retriever()
+        document_list, pages_list = collector.get_info(keywords)
 
-# info retrieving ('n' most relevant documents based on BM25)
-n = 5
-collector = Info_Retriever()
-document_list, pages_list = collector.get_info(keywords)
+        ranker = Info_Ranker(document_list)
+        top_documents = ranker.rank_documents(document_list, query, 10)
+        
+        DistilBert_answer_generator = DistilBertAnswerGenerator(top_documents,query)
+        answers_list = []
+        for i in range(len(top_documents)):
+            answer = DistilBert_answer_generator.generate_answer(top_documents[i],query)
+            
+            # non_empty_positions:
+            if answer.strip():
+                logging.info(f' Answer {i}: {answer}')
+                answers_list.append(answer)
+        
+        return answers_list
 
-ranker = Info_Ranker(document_list)
-top_documents = ranker.rank_documents(document_list, query, n)
+    except Exception as e:
+        logging.error(e)
+        return {"error": str(e)}
 
 
-# LLM: questions + answers
-DistilBert_answer_generator = DistilBertAnswerGenerator(top_documents,query)
-# # RoBERTa_answer_generator = RoBERTaAnswerGenerator()
-logging.info(' Answers are:')
+#############################################
+### WITHOUT API CONNECTION (PREVIOUS STEP)###
+#############################################
+## 1. user input
+# query = input("Any question? ")
 
-answers_list = []
-for i in range(len(top_documents)):
+## 2. keyword extraction from user input
+# kw = Keywords()
+# keywords = kw.extract_keywords(query)
 
-    answer = DistilBert_answer_generator.generate_answer(top_documents[i],query)
-    # answer = RoBERTa_answer_generator.generate_answer(top_documents[i], query)
-    answers_list.append(answer)
+## 3. info retrieving ('n' most relevant documents based on BM25)
+# n = 5
+# collector = Info_Retriever()
+# document_list, pages_list = collector.get_info(keywords)
 
-    print('Answer {}: {}'.format(i, answer))
-    logging.info(f' {i}{answer}')
+# ranker = Info_Ranker(document_list)
+# top_documents = ranker.rank_documents(document_list, query, n)
+
+## 4. LLM: questions + answers
+# DistilBert_answer_generator = DistilBertAnswerGenerator(top_documents,query)
+# # # RoBERTa_answer_generator = RoBERTaAnswerGenerator()
+# logging.info(' Answers are:')
+
+# answers_list = []
+# for i in range(len(top_documents)):
+#     answer = DistilBert_answer_generator.generate_answer(top_documents[i],query)
+#     # answer = RoBERTa_answer_generator.generate_answer(top_documents[i], query)
+#     answers_list.append(answer)
+
+# 5. ouput representation
+#     print('Answer {}: {}'.format(i, answer))
+#     logging.info(f' {i}{answer}')
+
+#############################################
+#############################################
